80eef02e91eb1caa706bbad09dbde25643bb174b
Geez-Us <noahsullivan382@gmail.com>
2023-03-03 21:22:26 -0800

    fix path route debug

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+//const notes = require('./public/assets/js/index.js');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+//app.use('/notes', notes);
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/notes', (req, res) =>
+    res.sendFile()
+)
+
+app.listen(3001, ()=>
+console.log(`app listening at http://localhost:3001`));
