880d72f050fac16f1f1fbb4e9c178b4a6e0247c6
Geez-Us <noahsullivan382@gmail.com>
2023-03-06 23:26:27 -0800

    able to pull data from db json using an api/notes get request on the server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+//const { getNotes } = require('./public/assets/js/index')
+const db = require('./db/db.json');
+const fs = require('fs');
 //const notes = require('./public/assets/js/index.js');
 const app = express();
 app.use(express.json());
@@ -7,13 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 //app.use('/notes', notes);
-
+app.get('/api/notes', (req, res) =>
+  res.json(db)
+)
 app.get('/notes', (req, res) => 
+  //getNotes(db),
+  //req.json(db),
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+  
 );
 
 app.post('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.json(db)
 );
 
 app.listen(3001, ()=>
